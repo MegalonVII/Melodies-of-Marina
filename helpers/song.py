25133c4cbb29e517d309cdb02a6fca6371c6d730
Megalon <megalonsba@gmail.com>
2024-03-04 00:32:24 -0500

    see that's fixed now something bc i forgot how markdown works

--- helpers/song.py
+++ helpers/song.py
@@ -10,6 +10,6 @@ class Song:
         embed = (discord.Embed(title='Now playing', description=f'**{self.source.title}**', color=discord.Color.green())
             .add_field( name='Duration', value=self.source.duration)
             .add_field(name='Requested by', value=self.requester.mention)
-            .add_field(name='URL', value=f"(Here)[{self.source.url}]")
+            .add_field(name='URL', value=f"[Link](<{self.source.url}>)")
             .set_thumbnail(url=self.source.thumbnail))
         return embed
