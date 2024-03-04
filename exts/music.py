f7ee4286bd307b3c0cbdf4b9b097d05a0f337daa
Megalon <megalonsba@gmail.com>
2024-03-04 01:57:16 -0500

    changed queue embed footer slightly

--- exts/music.py
+++ exts/music.py
@@ -138,7 +138,7 @@ class Music(commands.Cog):
         for i, song in enumerate(ctx.voice_state.songs[start:end], start=start):
             queue += f'`{i+1}.` **{song.source.title}** (*{song.source.duration}*){'\n' if i != end else ''}'
         embed = (discord.Embed(color=discord.Color.blue(), title=f'**{len(ctx.voice_state.songs)} songs:**', description=queue)
-                .set_footer(text=f'Viewing page {page}/{pages}\nTotal Queue Duration: {parse_total_duration([song.source.duration for song in ctx.voice_state.songs])}'))
+                .set_footer(text=f'Viewing page {page}/{pages}\nQueue length: {parse_total_duration([song.source.duration for song in ctx.voice_state.songs])}'))
         return await ctx.reply(embed=embed, mention_author=False)
 
     @commands.hybrid_command(name='shuffle', description="Shuffles the queue!")
